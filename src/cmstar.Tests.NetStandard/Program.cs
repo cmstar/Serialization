583142fb2818340544d9d8c483944d2cca40732c
cmstar <cosmo_sei@hotmail.com>
2019-11-08 16:43:35 +0800

    Supported .net standard 2.0 .

---
+++ src/cmstar.Tests.NetStandard/Program.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace cmstar.Tests.NetStandard
+{
+    public static class Program
+    {
+        [Test]
+        public static void Tmp()
+        {
+            Assert.Pass();
+        }
+    }
+}
